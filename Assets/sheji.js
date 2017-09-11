07d58da40cd89a8b4f14166583c9f2df6f40a4fe
15151450021 <31426110+15151450021@users.noreply.github.com>
2017-09-11 21:25:37 +0800

    a
    a

---
+++ Assets/sheji.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+var speed:int=5;
+var newobject:Transform;
+function Update () {
+	var x:float=Input.GetAxis("Horizontal")*Time.deltaTime*speed;
+	var z:float=Input.GetAxis("Vertical")*Time.deltaTime*speed;
+	transform.Translate(x,0,z);
+	if(Input.GetButtonDown("Fire1")){
+		var n:Transform=Instantiate(newobject,transform.position,transform.rotation);
+		
+		var fwd:Vector3=transform.TransformDirection(Vector3.forward);
+		n.rigidbody.AddForce(fwd*5000);
+		
+		
+	}	
+
+
+}
